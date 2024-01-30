d5f363e0c25ed6632ced61b5e58240e286076d0a
Francis <arojas.francisco@gmail.com>
2024-01-30 00:43:37 -0300

    added js functions

--- script.js
+++ script.js
@@ -12,8 +12,21 @@ function Book(title, author, pages, isRead ){
         return `The ${title} written by ${author} has ${pages} pages, ${isRead} `;
     }
 }
+const book1 = new Book('Roots',"rootsie", 450, 'yes');
+const book2 = new Book('Roots',"rootsie", 300, 'yes');
+const book3 = new Book('Roots',"rootsie", 50, 'no');
 
+myLibrary.push(book1);
+myLibrary.push(book2);
+myLibrary.push(book3);
 
 function addBookToLibrary(){
     
 }
+
+
+function displayBook (){
+    myLibrary.forEach((element) => console.log(element));
+        
+    };
+
