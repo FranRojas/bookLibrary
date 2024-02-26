644e7b06b2cb6585eeb9ca18518591c72aeb7c6a
Franci <arojas.francisco@gmail.com>
2024-02-26 18:43:44 -0300

    function is working, need to work on adding function and style

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const book1 = new Book('Roots',"rootsie", 450, 'yes');
 const book2 = new Book('Pride and Prejudice',"don", 300, 'yes');
 const book3 = new Book('How to think',"smartAI", 50, 'no');
 
-const myLibrary = [book1, book2, book3];
+const myLibrary = [];
 const rootElemento=document.querySelector(".display");
 
 
@@ -23,33 +23,31 @@ function Book(title, author, pages, isRead ){
 
 
 function addBookToLibrary(){
-    myLibrary.push(book344);
-    myLibrary.push(book34);
+    myLibrary.push(book1);
     console.log("add ");
-   5
+   
 }
 const BtnAdd =  document.querySelector(".btn-add");
-//BtnAdd.addEventListener("click", addBookToLibrary);
+BtnAdd.addEventListener("click", addBookToLibrary);
 
 
 
 
 function displayBook (){
    rootElemento.innerHTML = `
-   <div>
+
     ${myLibrary.map(function(book){
         return  `
         <div class= card>
-        <p> ${book.title}  </p>
-        <p> ${book.author }  </p>
-        <p> ${book.pages}  </p>
-        <p> ${book.isRead}  </p>
+        <p> ${book.title}</p>
+        <p> ${book.author}</p>
+        <p> ${book.pages}</p>
+        <p> ${book.isRead}</p>
        
         </div>
         `
     }).join('')}
    
-   </div> 
 
    <p> These ${myLibrary.length} books have been added recently </p>
    `
